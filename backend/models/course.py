1ef946f4f4ff6630d2a71c124f04db0cf8e8bce5
josfam <jamukun@proton.me>
2025-03-24 17:34:56 +0300

    Change column name for consistency

--- backend/models/course.py
+++ backend/models/course.py
@@ -13,13 +13,13 @@ class Course(Base):
     __tablename__ = 'courses'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    course_code = Column(String(30), nullable=False)
-    course_name = Column(String(120), nullable=False)
+    courseCode = Column(String(30), nullable=False)
+    courseName = Column(String(120), nullable=False)
 
     # relationship with students
     students = relationship('Student', back_populates='course')
 
     def __init__(self, course_code: str, course_name: str):
         """Initialize the Course instance"""
-        self.course_code = course_code
-        self.course_name = course_name
+        self.courseCode = course_code
+        self.courseName = course_name
