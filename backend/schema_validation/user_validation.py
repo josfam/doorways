4dd03b2365857301dbd65a20ebc80c3c24332bc6
josfam <jamukun@proton.me>
2025-03-25 15:23:44 +0300

    Add user validation with pydantic schema

---
+++ backend/schema_validation/user_validation.py
@@ -0,0 +1,13 @@
+"""Schema validation classes for user-related requests and responses"""
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    """Schema validation for creating a user"""
+
+    email: EmailStr
+    surname: str
+    given_name: str
+    phone_number: str
+    role_id: int
