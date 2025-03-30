12aaf1279f6c21e9121b7418fde0d249aa68859f
josfam <jamukun@proton.me>
2025-03-30 12:13:14 +0300

    Update role from 'security' to 'security_guard'

--- backend/api/v1/utils/roles.py
+++ backend/api/v1/utils/roles.py
@@ -5,6 +5,7 @@ from backend.schema_validation.user_validation import UserCreate
 from backend.models.user import User
 from backend.models.student import Student
 from backend.models.lecturer import Lecturer
+from backend.models.security_guard import SecurityGuard
 from backend.models.admin import Admin
 from backend.models.role import Role
 from .custom_exceptions import MissingUserAttributeError
@@ -34,5 +35,8 @@ def add_user_to_role_table(
     elif role_name == 'admin':
         admin = Admin(user_id=user.id)
         session.add(admin)
+    elif role_name == 'security_guard':
+        security_guard = SecurityGuard(user_id=user.id)
+        session.add(security_guard)
     else:
         pass
