06545e9f83c5ca5f073d508dd41a4b97e238f854
josfam <jamukun@proton.me>
2025-04-02 09:30:46 +0300

    Add tabbed UI for viewing different users

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,9 +1,46 @@
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@radix-ui/react-tabs'
+import StudentList from '../components/admin/StudentList'
+import LecturerList from '../components/admin/LecturerList'
+import SecurityGuardList from '../components/admin/SecurityGuardList'
+
 const AdminPage = () => {
   return (
-    <div>
-      <h1>Admin Page</h1>
-      <p>This is the admin page.</p>
-    </div>
+    <>
+      <div className="w-full bg-orange-300 p-2">
+        <h1 className="text-2xl font-bold">Admin</h1>
+      </div>
+      <div className="pt-20">
+        <div className="rounded-lg border-2 border-slate-400 bg-slate-400 p-10">
+          <Tabs
+            defaultValue="students"
+            className="flex flex-col gap-8 lg:w-[1400px]"
+          >
+            <div>
+              <TabsList className="grid w-full grid-cols-3 gap-6">
+                <TabsTrigger value="students" className="!bg-white">
+                  Students
+                </TabsTrigger>
+                <TabsTrigger value="lecturers" className="!bg-white">
+                  Lecturers
+                </TabsTrigger>
+                <TabsTrigger value="security-guards" className="!bg-white">
+                  Lecturers
+                </TabsTrigger>
+              </TabsList>
+            </div>
+            <TabsContent value="students">
+              <StudentList />
+            </TabsContent>
+            <TabsContent value="lecturers">
+              <LecturerList />
+            </TabsContent>
+            <TabsContent value="security-guards">
+              <SecurityGuardList />
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </>
   )
 }
 
