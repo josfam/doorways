f7b085717c221c88c9053076bf3b10a1881c5378
josfam <jamukun@proton.me>
2025-04-30 10:35:03 +0300

    Remove overflow for admin layout

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 // Container layout that wraps all admin pages
 const AdminLayout = () => {
   return (
-    <main className="flex h-full w-full flex-col items-center overflow-y-scroll">
+    <main className="flex h-full w-full flex-col items-center">
       <Outlet />
     </main>
   )
