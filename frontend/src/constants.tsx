ef75d0b1f356861c448091c0e7b804bfbad88a04
josfam <jamukun@proton.me>
2025-05-11 18:04:09 +0300

    Setup logic for the code countdown timer

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -1,4 +1,12 @@
 const baseAPIUrl = "http://127.0.0.1:8000/api/v1";
 const codesAPIUrl = `${baseAPIUrl}/codes`;
 
-export { baseAPIUrl, codesAPIUrl };
+const codeExpirationTime = 5; // seconds
+const expirationCountdownInterval = 1000; // milliseconds
+
+export {
+  baseAPIUrl,
+  codesAPIUrl,
+  codeExpirationTime,
+  expirationCountdownInterval,
+};
--- frontend/src/pages/CodeRequestPage.tsx
+++ frontend/src/pages/CodeRequestPage.tsx
@@ -1,11 +1,45 @@
 import { useQuery } from "@tanstack/react-query";
-import { codesAPIUrl } from "@/constants";
+import {
+  codesAPIUrl,
+  codeExpirationTime,
+  expirationCountdownInterval,
+} from "@/constants";
 
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CodeRequestPage = () => {
   const [code, setCode] = useState<string>("--");
+  const [expirationTime, setExpirationTime] =
+    useState<number>(codeExpirationTime);
+  const [timeLeft, setTimeLeft] = useState<number>(expirationTime);
+  const [codeWasRequested, setCodeWasRequested] = useState<boolean>(false);
+
+  // Timer for a countdown for expiration of the requested code
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (!codeWasRequested) return;
+
+    if (timeLeft >= 0) {
+      timer = setTimeout(() => {
+        setTimeLeft((prevTime) => {
+          if (prevTime <= 1) {
+            setCode("--");
+            setCodeWasRequested(false);
+            return codeExpirationTime;
+          }
+          return prevTime - 1;
+        });
+      }, expirationCountdownInterval);
+    }
+
+    // cleanup for the running timer
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [codeWasRequested, timeLeft]);
 
   const handleCodeRequest = async () => {
     const response = await fetch(`${codesAPIUrl}/random-code`, {
@@ -16,7 +50,9 @@ const CodeRequestPage = () => {
     });
     if (response.ok) {
       const data = await response.json();
+      setExpirationTime(data.expiration_time);
       setCode(data.random_code);
+      setCodeWasRequested(true);
     } else {
       console.error("Error requesting code:", response.statusText);
     }
@@ -27,8 +63,14 @@ const CodeRequestPage = () => {
       <div>
         <h1 className="text-4xl">Request a code</h1>
       </div>
-      <div className="flex h-64 w-64 items-center justify-center rounded-xl border-8 text-9xl">
-        {code}
+      <div className="flex flex-col">
+        {/* container for countdown timer */}
+        <div className="absolute mr-[-30px] mt-[-30px] flex h-20 w-20 items-center justify-center self-end rounded-full bg-emerald-400 text-center text-5xl font-bold text-emerald-900">
+          {timeLeft}
+        </div>
+        <div className="flex h-64 w-64 items-center justify-center rounded-xl border-8 text-9xl">
+          {code}
+        </div>
       </div>
       <Button className="h-16 w-64 p-4 text-xl" onClick={handleCodeRequest}>
         Request code
