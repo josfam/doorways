bb6cd5acbdfbd6f13e23fc2d7af48543f52c9ff1
josfam <jamukun@proton.me>
2025-05-21 13:59:12 +0300

    Add input box and fallback btn for code entry

--- frontend/src/pages/CodeEntryPage.tsx
+++ frontend/src/pages/CodeEntryPage.tsx
@@ -1,46 +1,54 @@
-import { useQuery } from "@tanstack/react-query";
-import { baseAPIUrl } from "@/constants";
+// import { useQuery } from "@tanstack/react-query";
+// import { baseAPIUrl } from "@/constants";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const CodeEntryPage = () => {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["enter-code"],
-    queryFn: async () => {
-      const url = `${baseAPIUrl}/codes/release-code/${code}`;
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    },
-  });
+  // const { data, isLoading, isError } = useQuery({
+  //   queryKey: ["enter-code"],
+  //   queryFn: async () => {
+  //     const url = `${baseAPIUrl}/codes/release-code/`;
+  //     const response = await fetch(url, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     if (!response.ok) {
+  //       throw new Error("Network response was not ok");
+  //     }
+  //     return response.json();
+  //   },
+  // });
 
-  const handleComplete = (code: string) => {
-    console.log(`Code entered: ${code}`);
-    // You can handle the code submission logic here
-  };
+  // const handleComplete = (code: string) => {
+  //   console.log(`Code entered: ${code}`);
+  //   // You can handle the code submission logic here
+  // };
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
 
-  if (isError) {
-    return <div>Error loading data</div>;
-  }
+  // if (isError) {
+  //   return <div>Error loading data</div>;
+  // }
 
   return (
     <div className="flex w-full flex-col items-center justify-center gap-40">
       <div>
-        <h1>Request code</h1>
+        <h1 className="page-header">Input your code</h1>
       </div>
-      <div className="mt-auto">
-        <Button></Button>
+      <div>
+        <Input
+          type="number"
+          autoFocus
+          className="h-32 w-[500px] border text-center !text-8xl [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+          inputMode="numeric"
+          pattern="[0-9]*"
+        />
+        <Button className="btn-cta btn-ter w-[500px]">Use my ID instead</Button>
       </div>
     </div>
   );
