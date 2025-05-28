1e8a3b60d075f4648190cabe0c1c9eeb51b60e7e
josfam <jamukun@proton.me>
2025-05-28 07:40:28 +0300

    Add more constants

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -2,10 +2,12 @@
 const baseAPIUrl = "http://127.0.0.1:8000/api/v1";
 const codesAPIUrl = `${baseAPIUrl}/codes`;
 const authAPIUrl = `${baseAPIUrl}/auth`;
+const baseWebSocketUrl = "ws://127.0.0.1:8000/api/v1";
 
 const codeExpirationTime = 5; // seconds
 const expirationCountdownInterval = 1000; // milliseconds
 const toastDuration = 2000; // milliseconds
+const inputResetDelay = 1000; // milliseconds
 
 const role_names = {
   admin: "admin",
@@ -16,10 +18,12 @@ const role_names = {
 
 export {
   baseAPIUrl,
+  baseWebSocketUrl,
   codesAPIUrl,
   codeExpirationTime,
   expirationCountdownInterval,
   authAPIUrl,
   toastDuration,
   role_names,
+  inputResetDelay,
 };
