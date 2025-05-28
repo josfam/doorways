2468f728e7294f55f435ffd39dd417ad581d6d67
josfam <jamukun@proton.me>
2025-05-28 12:59:14 +0300

    Tweak the styles of  both page layouts

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 // Container layout that wraps all admin pages
 const AdminLayout = () => {
   return (
-    <main className="flex h-full w-full flex-col items-center">
+    <main className="flex h-full w-full flex-col items-center justify-center">
       <Outlet />
     </main>
   );
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 // Container layout that wraps most pages
 const MainLayout = () => {
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center">
+    <main className="flex h-full w-full flex-col items-center justify-center bg-amber-50 p-4">
       <Outlet />
     </main>
   );
