a1c4ef84c3c3602f07059ff51cec313b5b10a4db
josfam <jamukun@proton.me>
2025-05-28 13:01:07 +0300

    Re-style the admin page

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -6,24 +6,24 @@ import SecurityGuardList from "../components/admin/SecurityGuardList";
 const AdminPage = () => {
   return (
     <>
-      <div className="w-full bg-orange-300 p-2">
-        <h1 className="text-2xl font-bold">Admin</h1>
+      <div className="fixed left-0 top-0 w-full bg-orange-300 p-2">
+        <h1 className="text-2xl font-bold text-orange-900">System Admin</h1>
       </div>
       <div className="pt-20">
-        <div className="h-[650px] rounded-lg border-2 border-slate-400 bg-slate-400 p-10">
+        <div className="h-[650px] rounded-lg border-2 border-slate-400 bg-slate-300 p-10">
           <Tabs
             defaultValue="students"
             className="flex flex-col gap-8 lg:w-[1400px]"
           >
             <div>
-              <TabsList className="grid w-full grid-cols-3 gap-6">
-                <TabsTrigger value="students" className="!bg-white">
+              <TabsList className="grid w-full grid-cols-3 gap-6 text-lg">
+                <TabsTrigger value="students" className="h-16 bg-white">
                   Students
                 </TabsTrigger>
-                <TabsTrigger value="lecturers" className="!bg-white">
+                <TabsTrigger value="lecturers" className="bg-white">
                   Lecturers
                 </TabsTrigger>
-                <TabsTrigger value="security-guards" className="!bg-white">
+                <TabsTrigger value="security-guards" className="bg-white">
                   Security Guards
                 </TabsTrigger>
               </TabsList>
