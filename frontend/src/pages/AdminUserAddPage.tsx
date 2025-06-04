c0b36fbd0f7809614658028d0e3bc2b04ea0b207
josfam <jamukun@proton.me>
2025-06-04 18:12:35 +0300

    Add placeholder sections for add and view users

---
+++ frontend/src/pages/AdminUserAddPage.tsx
@@ -0,0 +1,3 @@
+export const AdminUserAddPage = () => {
+  return <h1 className="text-2xl font-bold">Add users</h1>;
+};
---
+++ frontend/src/pages/AdminUserViewPage.tsx
@@ -0,0 +1,5 @@
+export const AdminUserViewPage = () => {
+  return (
+    <h1 className="font-bold text-2xl">View users</h1>
+  );
+};
