08e9d4b976a55609a4613823d21f0986c0008b8d
josfam <jamukun@proton.me>
2025-06-04 18:06:26 +0300

    Add Admin sidebar

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -1,10 +1,22 @@
 import { Outlet } from "react-router-dom";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AdminSidebar } from "@/components/admin-sidebar";
 
 // Container layout that wraps all admin pages
 const AdminLayout = () => {
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center">
-      <Outlet />
+    <main className="admin-layout flex h-dvh">
+      {/* admin sidebar */}
+      <div>
+        <SidebarProvider>
+          <AdminSidebar />
+          <SidebarTrigger />
+        </SidebarProvider>
+      </div>
+      {/* main content area */}
+      <section className="flex w-full flex-col items-center justify-center">
+        <Outlet />
+      </section>
     </main>
   );
 };
