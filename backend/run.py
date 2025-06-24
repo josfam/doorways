52f55d7723d6a08ce438a48a1e7fa8f27f585b07
josfam <jamukun@proton.me>
2025-06-24 15:37:20 +0300

    Fix: Add 127.0.0.1 variations for CORS

--- backend/run.py
+++ backend/run.py
@@ -24,6 +24,8 @@ app.include_router(main_router)
 origins = [
     'http://localhost:5173',
     'http://localhost:5173/',
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5173/',
 ]
 
 app.add_middleware(
