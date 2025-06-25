4c83f13b353efce08e5b23e395f98a8963fef5d3
josfam <jamukun@proton.me>
2025-06-25 03:59:51 +0300

    Fix: Add role name to role names

--- backend/api/v1/utils/constants.py
+++ backend/api/v1/utils/constants.py
@@ -6,4 +6,10 @@ class TransitionTypes(Enum):
     EXIT = 2
 
 
-role_names = {"sys admin": 1, "lecturer": 2, "student": 3, "security guard": 4}
+role_names = {
+    "sys admin": 1,
+    "lecturer": 2,
+    "student": 3,
+    "security guard": 4,
+    "admin": 5,
+}
