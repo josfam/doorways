c3b6294e99df610b4c98879a394985cffd195bcf
josfam <jamukun@proton.me>
2025-06-30 20:30:08 +0300

    Fix main layout height

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 // Container layout that wraps most pages
 const MainLayout = () => {
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center bg-amber-50 p-4">
+    <main className="flex h-max w-full flex-col items-center justify-center bg-amber-50 p-4">
       <Outlet />
     </main>
   );
