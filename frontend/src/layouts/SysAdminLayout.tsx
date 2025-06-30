22f43a58eb7617bc66bfede7588557eae0a578aa
josfam <jamukun@proton.me>
2025-06-30 20:09:37 +0300

    Add footer with github link to repo

--- frontend/src/layouts/SysAdminLayout.tsx
+++ frontend/src/layouts/SysAdminLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AdminSidebar } from "@/components/admin-sidebar";
+import githubLogo from "@/assets/icons/github.svg";
 
 // Container layout that wraps all admin pages
 const SysAdminLayout = () => {
@@ -17,7 +18,16 @@ const SysAdminLayout = () => {
       <section className="relative mb-48 flex h-max w-full flex-col items-center gap-6 px-4 pt-12 sm:px-8 sm:pt-14">
         <Outlet />
         <footer className="fixed bottom-0 flex h-16 w-full items-center justify-center rounded-t-lg bg-slate-200">
-          <p className="text-lg text-slate-700">Copyright 2025</p>
+          <p className="flex items-center justify-center gap-6 text-lg text-slate-700">
+            <a href="https://github.com/josfam/doorways" target="blank_">
+              <img
+                src={githubLogo}
+                alt="github logo that links to the project github repository"
+                className="h-8 w-8"
+              />
+            </a>
+            Copyright 2025
+          </p>
         </footer>
       </section>
     </main>
